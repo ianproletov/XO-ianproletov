ae938b34c4ea3510202cbd66652cd4fdba21b1f3
ian proletov <yan.proletov@gmail.com>
2020-04-05 17:10:41 +0300

    PlayerTest create

---
+++ test/ru/proletov/xo/model/PlayerTest.java
@@ -0,0 +1,31 @@
+package ru.proletov.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetName() throws Exception {
+        final String inputValue = "Ian";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
